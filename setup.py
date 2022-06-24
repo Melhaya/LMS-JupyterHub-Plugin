8e4c05dad1bebfbd8d798fca979df8d27f4b14d5
Mohamed Elhayany <40873107+Melhaya@users.noreply.github.com>
2022-06-24 11:50:01 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="LTI_Score_Plugin",
+    version='0.1.0',
+    url="https://github.com/Melhaya/LMS-JupyterHub-Plugin",
+    author="Mohamed Elhayany",
+    description="Jupyter Notebook Plugin for submitting score to LMS through LTI",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'bs4',
+        'gitpython',
+        'requests'
+    ],
+    package_data={'LTI_Score_Plugin': ['static/*']},
+)
